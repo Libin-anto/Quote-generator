4a0ac65331d01bf55010b5b2f4c6088631ed65a1
Libin-anto <libinantoe.aids2024@citchennai.net>
2025-07-02 16:14:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+quotes = [
+    "Hardwork never betrays you."
+    "CGPA is just a myth - Unkown."
+    "Failure is about winning."
+]
+
+print("💬 Quote of the Day:")
+print(random.choice(quotes))
